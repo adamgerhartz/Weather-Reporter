17c12610b6ae60961fc1539450907927546aedaa
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 16:39:16 -0700

    Test

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-import WeatherController from './weather-main/WeatherController.js';
-
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => {
 		console.log('Received a request for the root directory')
-		const weatherController = new WeatherController('#weather');
-		console.log("Did we make it?");
-		weatherController.init();
 		res.render('pages/index')
 	})
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
