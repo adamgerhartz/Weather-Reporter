d35ef3826e6c511a66c510f8a034ed3a762e8082
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 14:03:02 -0700

    Add response to the root endpoint

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => {
+		console.log('Received a request for the root directory')
+		res.render('pages/index')
+	})
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
