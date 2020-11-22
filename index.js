77a01f8d595329f77769fdf32eeb90921760e609
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 17:33:05 -0700

    Test

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000;
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
-	.set('controllers', path.join(__dirname, 'controllers'))
-	.set('models', path.join(__dirname, 'models'))
+	// .set('controllers', path.join(__dirname, 'controllers'))
+	// .set('models', path.join(__dirname, 'models'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => {
 		console.log('Received a request for the root directory')
