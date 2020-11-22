c215a183c973a9965ba6e958495358f4ddd9c5ae
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 20:55:14 -0700

    Test

--- weather-main/Weather.js
+++ weather-main/Weather.js
@@ -4,14 +4,14 @@ const utils = require('./utilities');
 module.exports = class Weather {
   constructor() {
     this.baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-    this._weather = [];
+    this._weather = '';
   }
   async getWeatherByCity(city = 'Detroit') {
     // use the getJSON function and the position provided to build out the correct URL to get the data we need.  Store it into this._quakes, then return it
     const apiKey = process.env.API_KEY_WEATHER;
     
     this._weather = utils.getJSON(`${this.baseUrl}?q=${city}&apiKey=${apiKey}`);
-    console.log(this._weather);
+    console.log(`Weather Response: ${this._weather}`);
     return this._weather;
   }
 
