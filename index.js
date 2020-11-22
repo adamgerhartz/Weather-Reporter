9fd0aad4198c5eee7a51f20818f15cda8a48fbbe
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 23:24:27 -0700

    Shimmy layers down one layer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-const WeatherModel = require('./api/Weather');
+const WeatherModel = require('./api/Weather.js');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const defaultCity = 'New York';
