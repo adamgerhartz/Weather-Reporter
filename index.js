b0fae95c7637d1a091f846e7ad8d1e222e6a8771
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 20:37:52 -0700

    Test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const WeatherController = require('./weather-main/WeatherController');
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
+console.log(process.env);
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
-	// .set('controllers', path.join(__dirname, 'controllers'))
-	// .set('models', path.join(__dirname, 'models'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => {
 		console.log('Received a request for the root directory')
