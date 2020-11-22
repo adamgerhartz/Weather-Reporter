11bf14eec0b2ebc614be098ef219f98f9afcab37
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-21 20:29:04 -0700

    Test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ejs = require('ejs');
 const WeatherController = require('./weather-main/WeatherController');
 const PORT = process.env.PORT || 5000;
-console.log(process.env.API_KEY);
+
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
@@ -15,6 +15,7 @@ express()
 	.get('/', (req, res) => {
 		console.log('Received a request for the root directory')
 		const weatherController = new WeatherController('Detroit');
+		console.log(process.env.API_KEY);
 		weatherController.init();
 		res.render('pages/index')
 	})
--- weather-main/utilities.js
+++ weather-main/utilities.js
@@ -2,21 +2,6 @@ const getLocation = function(options) {
     const unirest = require("unirest");
     return new Promise(function(resolve, reject) {
         // return geolocation
-        var req = unirest("GET", "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/");
-
-        console.log(process.env.API_KEY);
-
-        req.headers({
-        "x-rapidapi-key": process.env.API_KEY,
-        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-        "useQueryString": true
-        });
-
-        req.end(function (res) {
-        if (res.error) throw new Error(res.error);
-
-          console.log(res.body);
-        });
     });
 };
    
