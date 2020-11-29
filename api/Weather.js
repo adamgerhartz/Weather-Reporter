b3c304412a4abd5a24dc2ff1f512ca985747d164
Adam Gerhartz <adamgerhartz@Adams-Mac-mini.local>
2020-11-29 01:46:59 -0700

    Tests

--- api/Weather.js
+++ api/Weather.js
@@ -6,7 +6,7 @@ module.exports = class Weather {
     this.baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
     this._weather = '';
   }
-  async getWeatherByCity(city = 'Detroit', callback) {
+  getWeatherByCity(city = 'Detroit', callback) {
     const apiKey = process.env.API_KEY_WEATHER;
     city = city.replace(/-/g, ' ');
     this._weather = utils.getJSON(`${this.baseUrl}?q=${city}&apiKey=${apiKey}&units=imperial`);
