986854cdadfb410637accde37df37cf2dbdee372
Adam Gerhartz <adamgerhartz@Adams-Mac-mini.local>
2020-12-06 02:19:46 -0700

    Test

--- public/views/WeatherView.js
+++ public/views/WeatherView.js
@@ -57,8 +57,8 @@ export default class WeatherView {
 		const array = [];
 		items.forEach(item => {
 			array.push({
-				y: item.temp,
-				x: `${item.date.toString().slice(16, 24)}`
+				y: item.temp
+				// x: `${item.date.toString().slice(16, 24)}`
 			});
 		})
 		return array;
