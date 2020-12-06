7c97bf1cbea122b555e31a06d52eac97dd04f960
Adam Gerhartz <adamgerhartz@Adams-Mac-mini.local>
2020-12-06 02:39:09 -0700

    Test

--- public/views/WeatherView.js
+++ public/views/WeatherView.js
@@ -68,6 +68,15 @@ export default class WeatherView {
 				})
 			});
 			chart.render();
+
+			function toogleDataSeries(e){
+				if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+					e.dataSeries.visible = false;
+				} else{
+					e.dataSeries.visible = true;
+				}
+				chart.render();
+			}
 		});
 		
 		
