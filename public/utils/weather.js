e492e80a166c8f12d96301eb19cdfd09ba5854bd
Adam Gerhartz <adamgerhartz@Adams-Mac-mini.local>
2020-12-14 11:04:45 -0700

    Test Geolocation

--- public/utils/weather.js
+++ public/utils/weather.js
@@ -3,7 +3,7 @@ import WeatherController from "../controllers/WeatherController.js";
 const weatherController = new WeatherController('main');
 window.addEventListener('load', ()=> {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPostion(weatherController.showWeatherForm(position));
+		navigator.geolocation.getCurrentPostion(weatherController.showWeatherForm());
 	} else {
 		weatherController.showWeatherForm();	
 	}
